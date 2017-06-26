561f025c33de92230f25c03df9e9d04d7f6d4cb4
Greg Dorrell <Greg@Dorrell.me>
2017-06-26 19:13:43 +0100

    Remove some unneeded test infrastructure

--- server/src/test/scala/me/gregd/cineworld/CinemaServiceTest.scala
+++ server/src/test/scala/me/gregd/cineworld/CinemaServiceTest.scala
@@ -4,7 +4,7 @@ import java.time.LocalDate
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import fakes.{FakeCineworldRepository, FakeRatings, NoOpCache}
+import fakes.{FakeRatings, NoOpCache}
 import me.gregd.cineworld.config.values.{TmdbKey, TmdbRateLimit}
 import me.gregd.cineworld.dao.TheMovieDB
 import me.gregd.cineworld.dao.cinema.cineworld.CineworldCinemaDao
@@ -14,7 +14,7 @@ import me.gregd.cineworld.dao.cinema.vue.raw.VueRepository
 import me.gregd.cineworld.dao.movies.Movies
 import me.gregd.cineworld.util.FixedClock
 import monix.execution.Scheduler
-import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Span}
 import org.scalatest.{FunSuite, Matchers}
 import play.api.libs.ws.ahc.AhcWSClient
@@ -22,9 +22,9 @@ import stub.Stubs
 
 import scala.concurrent.duration._
 
-class CinemaServiceTest extends FunSuite with ScalaFutures with Matchers with IntegrationPatience {
+class CinemaServiceTest extends FunSuite with ScalaFutures with Matchers {
 
-  implicit val defaultPatienceConfig = PatienceConfig(Span(1500, Millis))
+  implicit val defaultPatienceConfig = PatienceConfig(Span(3000, Millis))
 
   val wsClient = AhcWSClient()(ActorMaterializer()(ActorSystem()))
   val tmdb = new TheMovieDB(TmdbKey(""), wsClient, Stubs.tmdb.baseUrl, NoOpCache.cache, Scheduler.global, TmdbRateLimit(1.second, 1000))
@@ -32,8 +32,8 @@ class CinemaServiceTest extends FunSuite with ScalaFutures with Matchers with In
 
   val movieDao = new Movies(tmdb, FakeRatings)
   val cineworldRaw = new CineworldRepository(wsClient, NoOpCache.cache, Stubs.cineworld.baseUrl)
-  val cineworldDao = new CineworldCinemaDao(movieDao, tmdb, FakeCineworldRepository)
-  val clock = FixedClock(LocalDate.parse("2017-03-23"))
+  val cineworldDao = new CineworldCinemaDao(movieDao, tmdb, cineworldRaw)
+  val clock = FixedClock(LocalDate.parse("2017-05-23"))
   val vueDao = new VueCinemaDao(repo, movieDao, clock)
 
   val cinemaService = new CinemaService(movieDao, cineworldDao, vueDao, clock)
@@ -43,13 +43,13 @@ class CinemaServiceTest extends FunSuite with ScalaFutures with Matchers with In
 
     val res = cinemaService.getMoviesAndPerformances("1010882", "today").futureValue.toSeq
 
-    res.size shouldBe 2
+    res.size shouldBe 11
 
     val (movie, performances) = res.head
 
-    movie.title shouldBe "Beauty And The Beast"
+    movie.title shouldBe "Guardians Of The Galaxy Vol. 2"
 
-    performances.size shouldBe 2
+    performances.size shouldBe 4
   }
 
 }

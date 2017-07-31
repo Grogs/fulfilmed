c9c7ed580cbea94eefdc6ffee8ef53b9cc51463a
Greg Dorrell <Greg@Dorrell.me>
2017-07-31 21:36:35 +0100

    finished akka-http stubs

--- server/src/test/scala/stub/Stubs.scala
+++ server/src/test/scala/stub/Stubs.scala
@@ -3,20 +3,12 @@ package stub
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.model.ContentTypes.`application/json`
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.DebuggingDirectives
 import akka.stream.ActorMaterializer
 import me.gregd.cineworld.config.values.{CineworldUrl, OmdbUrl, TmdbUrl, VueUrl}
-import play.api.http.ContentTypes.JSON
-import play.api.http.{DefaultFileMimeTypesProvider, FileMimeTypesConfiguration}
-import play.api.mvc.{ActionBuilder, AnyContent, Request}
-import play.api.mvc.Results.{NotFound, Ok}
-import play.api.routing.Router
-import play.api.routing.sird._
-import play.api.{BuiltInComponents, NoHttpFiltersComponents}
-import play.core.server._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -25,137 +17,102 @@ import scala.util.Try
 
 object Stubs {
 
-  private implicit val fileMimeTypes = new DefaultFileMimeTypesProvider(FileMimeTypesConfiguration()).get
-
-  private def return404(implicit action: ActionBuilder[Request, AnyContent]): Router.Routes = {
-    case other => action {
-      println(s"Returning 404 for ${other.uri}")
-      NotFound
-    }
-  }
-
-  private lazy val server2 = {
+  private lazy val server = {
     implicit val system = ActorSystem("http-stubs")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
 
     import akka.http.scaladsl.Http
 
-    val notFound: Route = DebuggingDirectives.logRequest(("Not found", Logging.InfoLevel))(complete("Failed"))
+    val notFound: Route = DebuggingDirectives.logRequest(("Not found", Logging.ErrorLevel))(complete("Failed"))
 
-    val route: Route = tmdb.route ~ notFound
+    val route: Route = tmdb.route ~ cineworld.route ~ vue.route ~ omdb.route ~ notFound
     Await.result(Http().bindAndHandle(route, "localhost", 0), 10.seconds)
   }
 
-  lazy val serverBase2 = s"http://127.0.0.1:${server2.localAddress.getPort}"
-
-  private lazy val server =
-    new AkkaHttpServerComponents with BuiltInComponents with NoHttpFiltersComponents {
-      implicit lazy val action = defaultActionBuilder
-      override lazy val serverConfig = ServerConfig(port = Some(0))
-      override lazy val actorSystem: ActorSystem = ActorSystem("stubbed-services")
-      val return404: Router.Routes = {
-        case other =>
-          action {
-            println(s"Returning 404 for ${other.uri}")
-            NotFound
-          }
-      }
-      lazy val router = Router.from(
-        tmdb.routes orElse cineworld.routes orElse vue.routes orElse omdb.routes orElse return404
-      )
-    }.server
-
-  lazy val serverBase = s"http://127.0.0.1:${server.httpPort.get}"
-
-  private def jsonResp(json: String) = HttpEntity(`application/json`, json)
+  private lazy val serverBase = s"http://127.0.0.1:${server.localAddress.getPort}"
 
   object tmdb {
 
-    val route: Route = {
-      (get & pathPrefix("3" / "movie")) {
-        (path("now_playing") & parameter('api_key, 'page)) { (key, page) =>
-            complete(
-              HttpEntity(`application/json`,
-                Source.fromResource(s"tmdb/now_playing-$page.json").mkString
-              )
-            )
-
-        } ~ path(Segment / "alternative_titles") { id =>
-          complete(
-            HttpEntity(`application/json`,
-              Try {
-                Source.fromResource(s"tmdb/alternate-titles-$id.json").getLines().mkString
-              }.getOrElse(s"""{"id":$id,"titles":[]}""")
-            )
+    val route: Route =
+      (get & path("3" / "movie" / "now_playing") & parameter('api_key, 'page)) { (_, page) =>
+        complete(
+          HttpEntity(`application/json`,
+            Source.fromResource(s"tmdb/now_playing-$page.json").mkString
           )
-        } ~ (path(LongNumber) & parameter('api_key)) { (id, apiKey) =>
-          complete(
-            HttpEntity(`application/json`,
-              """{"imdb_id":"tt7777777"}"""
-            )
+        )
+      } ~ (get & path("3" / "movie" / Segment / "alternative_titles")) { id =>
+        complete(
+          HttpEntity(`application/json`,
+            Try {
+              Source.fromResource(s"tmdb/alternate-titles-$id.json").mkString
+            }.getOrElse(s"""{"id":$id,"titles":[]}""")
           )
-        }
-
+        )
+      } ~ (get & path("3" / "movie" / LongNumber) & parameter('api_key)) { (_, _) =>
+        complete(
+          HttpEntity(`application/json`,
+            """{"imdb_id":"tt7777777"}"""
+          )
+        )
       }
-    }
 
-    def routes(implicit action: ActionBuilder[Request, AnyContent]): Router.Routes = {
-      case GET(p"/3/movie/now_playing" ? q"api_key=$key" & q"language=$_" & q"page=$page" & q"region=$_") =>
-        action {
-          Ok.sendResource(s"tmdb/now_playing-$page.json").as(JSON)
-        }
-      case GET(p"/3/movie/$id/alternative_titles" ? q"api_key=$_") =>
-        action {
-          Try {
-            Ok.sendResource(s"tmdb/alternate-titles-$id.json").as(JSON)
-          } getOrElse Ok(s"""{"id":$id,"titles":[]}""").as(JSON)
-        }
-      case GET(p"/3/movie/$tmdbId" ? q"api_key=$_") =>
-        action {
-          Ok("""{"imdb_id":"tt7777777"}""").as(JSON)
-        }
-    }
-
-    lazy val baseUrl = TmdbUrl(serverBase2)
+    lazy val baseUrl = TmdbUrl(serverBase)
   }
 
   object cineworld {
-    def routes(implicit action: ActionBuilder[Request, AnyContent]): Router.Routes = {
-      case GET(p"/getSites" ? q"json=$_" & q"max=$max") =>
-        action {
-          Ok.sendResource("cineworld/cinemas.json")
-        }
-      case GET(p"/pgm-site" ? q"si=$cinemaId" & q"max=$max") =>
-        action {
-          Ok.sendResource(s"cineworld/listings-$cinemaId.json")
-        }
-    }
+
+    val route: Route =
+      (get & path("getSites") & parameter('json, 'max)) { (_, max) =>
+        complete(
+          HttpEntity(`application/json`,
+            Source.fromResource("cineworld/cinemas.json").mkString
+          )
+        )
+      } ~ (get & path("pgm-site") & parameter('si, 'max)) { (cinemaId, _) =>
+        complete(
+          HttpEntity(`application/json`,
+            Source.fromResource(s"cineworld/listings-$cinemaId.json").mkString
+          )
+        )
+      }
 
     lazy val baseUrl = CineworldUrl(serverBase)
   }
 
   object vue {
-    def routes(implicit action: ActionBuilder[Request, AnyContent]): Router.Routes = {
-      case GET(p"/data/locations") =>
-        action {
-          Ok.sendResource("vue/locations.json")
-        }
-      case GET(p"/data/filmswithshowings/10032") =>
-        action {
-          Ok.sendResource("vue/filmswithshowings-10032.json")
-        }
-    }
+
+    val route: Route =
+      (get & path("data" / "locations"/)) {
+        complete(
+          HttpEntity(`application/json`,
+            Source.fromResource("vue/locations.json").mkString
+          )
+        )
+      } ~ (get & path("data" / "filmswithshowings" / LongNumber)) { id =>
+        complete(
+          id match {
+            case 10032 => HttpEntity(`application/json`,
+              Source.fromResource("vue/filmswithshowings-10032.json").mkString
+            )
+            case 1010882 =>
+              HttpEntity(`application/json`,"""{"Response":"False","Error":"Error getting data."}""")
+          }
+        )
+      }
 
     lazy val baseUrl = VueUrl(serverBase)
   }
 
   object omdb {
-    def routes(implicit action: ActionBuilder[Request, AnyContent]): Router.Routes = {
-      case GET(p"/" ? q"i=$imdbId" & q"apikey=$_") =>
-        action {
-          Ok.sendResource(s"omdb/$imdbId.json")
+    val route = {
+      (get & parameter('i, 'apikey)) { (imdbId, _) =>
+        complete {
+          HttpEntity(`application/json`,
+            Source.fromResource(s"omdb/$imdbId.json").mkString
+          )
         }
+      }
     }
 
     lazy val baseUrl = OmdbUrl(serverBase)

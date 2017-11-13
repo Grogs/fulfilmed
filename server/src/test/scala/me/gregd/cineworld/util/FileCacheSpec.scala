05decebb3a13df6376744292fc960ff11df083bd
Greg Dorrell <Greg@Dorrell.me>
2017-11-13 15:38:40 +0000

    Improve FileCacheSpec.

--- server/src/test/scala/me/gregd/cineworld/util/FileCacheSpec.scala
+++ server/src/test/scala/me/gregd/cineworld/util/FileCacheSpec.scala
@@ -3,19 +3,27 @@ package me.gregd.cineworld.util
 import java.nio.file.Files
 
 import org.scalatest.concurrent.{Eventually, IntegrationPatience, ScalaFutures}
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{BeforeAndAfterEach, FunSuite, Matchers}
 
 import scala.collection.JavaConverters._
+import scala.concurrent.Future
 import scalacache.AnyRefBinaryCodec
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class FileCacheSpec extends FunSuite with Matchers with ScalaFutures with Eventually with IntegrationPatience{
+class FileCacheSpec extends FunSuite with Matchers with ScalaFutures with Eventually with IntegrationPatience with BeforeAndAfterEach {
+
+  val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
+  val cache = new FileCache(cacheDir.toString)
+  def files = Files.list(cacheDir).iterator().asScala.toList
+
+  override def afterEach(): Unit = {
+    val remove = cache.removeAll()
+    super.afterEach()
+    remove.futureValue
+  }
 
   test("remove") {
-    val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
-    val cache = new FileCache(cacheDir.toString)
-    def files = Files.list(cacheDir).iterator().asScala.toList
-
     files shouldBe empty
 
     cache.put("a", 1, None)
@@ -32,20 +40,12 @@ class FileCacheSpec extends FunSuite with Matchers with ScalaFutures with Eventu
   }
 
   test("put without ttl") {
-    val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
-    val cache = new FileCache(cacheDir.toString)
-    def files = Files.list(cacheDir).iterator().asScala.toList
-
     files shouldBe empty
     cache.put("someKey", 1, None).futureValue shouldBe (())
     files.size shouldBe 1
   }
 
   test("put with ttl") {
-    val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
-    val cache = new FileCache(cacheDir.toString)
-    def files = Files.list(cacheDir).iterator().asScala.toList
-
     files shouldBe empty
 
     val write = System.currentTimeMillis()
@@ -54,66 +54,39 @@ class FileCacheSpec extends FunSuite with Matchers with ScalaFutures with Eventu
 
     files.size shouldBe 1
 
-    eventually(timeout(1.second)){
+    eventually(timeout(1.second)) {
       files.size shouldBe 0
       val timeLived = System.currentTimeMillis() - write
-      timeLived shouldBe > (300L)
-      timeLived shouldBe < (600L)
+      timeLived shouldBe >(300L)
+      timeLived shouldBe <(600L)
     }
   }
 
   test("removeAll") {
-    val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
-    val cache = new FileCache(cacheDir.toString)
-    def files = Files.list(cacheDir).iterator().asScala.toList
-
     files shouldBe empty
 
-    cache.put("a", 1, None)
-    cache.put("b", 2, None)
-    cache.put("c", 3, None)
-    cache.put("d", 4, None)
+    val inserts = Seq(
+      cache.put("a", 1, None),
+      cache.put("b", 2, None),
+      cache.put("c", 3, None),
+      cache.put("d", 4, None)
+    )
 
-    eventually(timeout(1.second)) {
-      files.size shouldBe 4
-    }
+    Future.sequence(inserts).futureValue
 
-    cache.removeAll()
+    files.size shouldBe 4
 
-    eventually(timeout(1.second)) {
-      files.size shouldBe 4
-    }
+    cache.removeAll().futureValue
+
+    files.size shouldBe 0
   }
 
   test("get") {
-    val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
-    val cache = new FileCache(cacheDir.toString)
-    def files = Files.list(cacheDir).iterator().asScala.toList
-
     cache.get[Int]("someKey").futureValue shouldBe None
 
     cache.put("someKey", 42, None).futureValue
 
     cache.get[Int]("someKey").futureValue shouldBe Some(42)
-    cache.get[String]("someKey").failed.futureValue shouldBe a [Throwable]
+    cache.get[String]("someKey").failed.futureValue shouldBe a[Throwable]
   }
-
-  test("close") {
-    val cacheDir = Files.createTempDirectory("FileCacheSpec").toAbsolutePath
-    val cache = new FileCache(cacheDir.toString)
-    def files = Files.list(cacheDir).iterator().asScala.toList
-
-    //Queue up some async actions
-    cache.put("a", 1, None)
-    cache.put("b", 2, None)
-    cache.put("c", 3, None)
-    cache.put("d", 4, None)
-    cache.removeAll()
-
-    //Close should return after removeAll is processed
-    cache.close()
-
-    files shouldBe empty
-  }
-
 }
